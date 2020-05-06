f6f1b616e9ef9272c373672bd4b4ce68659bc2e6
Guardstyle <59020563+Guardstyle@users.noreply.github.com>
2020-05-06 12:21:22 +0800

    I made the code simplier
    -I made the month array in 1 line rather than 12 line

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-/*
-ID: littlef6
-TASK: friday
-LANG: C++
-*/
-
 #include <bits/stdc++.h>
 #include <fstream>
 
@@ -17,22 +11,11 @@ int main()
     ifstream fin ("friday.in");
     ofstream fout ("friday.out");
 
-    int n, day[7], index = 0, month[12];
+    int n, day[7], index = 0;
+    int month[] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     for(int i = 0; i < 7; i++){
         day[i] = 0;
     }
-    month[0] = 31;
-    month[1] = 28;
-    month[2] = 31;
-    month[3] = 30;
-    month[4] = 31;
-    month[5] = 30;
-    month[6] = 31;
-    month[7] = 31;
-    month[8] = 30;
-    month[9] = 31;
-    month[10] = 30;
-    month[11] = 31;
     fin >> n;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < 12; j++){
